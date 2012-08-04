1471ee015d8298943b016f288b02851c3294cc85
Dominik Schadow <dominikschadow@googlemail.com>
2012-08-04 13:07:30 +0200

    Correct emp_id for Bob
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- demo/src/test/java/de/bit/camel/security/TestBob.java
+++ demo/src/test/java/de/bit/camel/security/TestBob.java
@@ -5,7 +5,7 @@ import de.bit.camel.security.services.EmpInfoService;
 public class TestBob extends TestUtils {
     public static void main(String[] args) {
         EmpInfoService client = createClient();
-        String bob = client.getEmployeeInformation(10001);
+        String bob = client.getEmployeeInformation(10002);
         
         System.out.println("EMP " + bob);
     }
