1410a10b964890b4235818469841390dcaf86119
Dominik Schadow <dominikschadow@googlemail.com>
2012-08-05 09:32:16 +0200

    Fixed tests
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- demo/src/test/java/de/bit/camel/security/TestEve.java
+++ demo/src/test/java/de/bit/camel/security/TestEve.java
@@ -5,11 +5,11 @@ import de.bit.camel.security.services.EmpInfoService;
 public class TestEve extends TestUtils {
     public static void main(String[] args) {
         EmpInfoService client = createClient();
-        String alice = client.getEmployeeInformation("10001");
+        String alice = client.getEmployeeInformation(10001);
         
         System.out.println("EMP " + alice);
         
-        String bob = client.getEmployeeInformation("10002");
+        String bob = client.getEmployeeInformation(10002);
         
         System.out.println("EMP " + bob);
     }
